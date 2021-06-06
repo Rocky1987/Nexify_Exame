9900aba9f6ff74defb62916eca3ab13ab71d9ab2
DESKTOP-H36UF2N\decie <g0952772916@yahoo.com.tw>
2021-06-06 11:41:00 +0800

    Nexify Project V2

--- Nextfy.Web/Controllers/UserController.cs
+++ Nextfy.Web/Controllers/UserController.cs
@@ -29,59 +29,7 @@ namespace Nextfy.Web.Controllers
             string cnStr = "data source=" + dbPath;
             if (!System.IO.File.Exists(dbPath))
             {
-                this.uerRepository.Creat();
-                //List<UserDataModel> userDataModelList = new List<UserDataModel>();
-
-                //UserDataModel a = new UserDataModel
-                //{
-                //    Name = "ASD",
-                //    DateOfBirth = new DateTime(2019, 5, 13),
-                //    Salary = 40000,
-                //    Address = "ASD"
-                //};
-
-                //userDataModelList.Add(a);
-                //UserDataModel b = new UserDataModel
-                //{
-                //    Name = "Jay",
-                //    DateOfBirth = new DateTime(1995, 7, 21),
-                //    Salary = 100000,
-                //    Address = "新北市"
-                //};
-
-                //userDataModelList.Add(b);
-                //UserDataModel c = new UserDataModel
-                //{
-                //    Name = "Leo",
-                //    DateOfBirth = new DateTime(1993, 7, 1),
-                //    Salary = 50000,
-                //    Address = "新北市"
-                //};
-
-                //userDataModelList.Add(c);
-                //UserDataModel d = new UserDataModel
-                //{
-                //    Name = "Test",
-                //    DateOfBirth = new DateTime(2019, 5, 13),
-                //    Salary = 50000,
-                //    Address = "台北市"
-                //};
-
-                //userDataModelList.Add(d);
-         
-                //UserDataModel e = new UserDataModel
-                //{
-                //    Name = "ZXC",
-                //    DateOfBirth = new DateTime(2019, 5, 13),
-                //    Salary = 40000,
-                //    Address = "ZXC"
-                //};
-
-                //userDataModelList.Add(e);
-
-                //foreach (UserDataModel item in userDataModelList) {
-                //    this.uerRepository.Add(item);
-                //}
+                this.uerRepository.Creat();               
             }
 
             //IEnumerable<UserDataModel> users = this.uerRepository.Query();
